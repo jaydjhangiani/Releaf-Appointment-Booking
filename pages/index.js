2dd3419a9639fe7af2d320c47c0a571246771422
Jay Jhangiani <jaydjhangiani@gmail.com>
2021-08-10 11:25:46 +0530

    added card sections

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer';
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard';
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
-export default function Home({exploreData}) {
+export default function Home({exploreData, cardData}) {
   return (
     <div className="">
       <Head>
@@ -15,6 +18,7 @@ export default function Home({exploreData}) {
     <Banner/>
 
     <main className="max-w-7xl mx-auto px-8 sm:px-16">
+
       <section className="pt-6">
         <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -24,7 +28,25 @@ export default function Home({exploreData}) {
         ))}
         </div>
       </section>
+
+      <section>
+          <h2 className = "text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className= "flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3 " >
+            {cardData?.map((item,index) => (
+              <MediumCard key={index} img={item.img} title={item.title} />
+            ))}
+          </div>
+      </section>
+
+      <LargeCard 
+        img="https://links.papareact.com/4cj"
+        title="The Greatest Outdoors"
+        description="Wishlits curated by Airbnb."
+        buttonText="Get Inspired"
+      />
+    
     </main>
+    <Footer/>
     </div>
   )
 } 
@@ -34,10 +56,16 @@ export async function getStaticProps() {
   then(
     (res) => res.json()
   );
+
+  const cardData = await fetch('https://links.papareact.com/zp1').
+  then(
+    (res) => res.json()
+  );
   
   return{
     props: {
-      exploreData
+      exploreData,
+      cardData
     }
   }
 }
